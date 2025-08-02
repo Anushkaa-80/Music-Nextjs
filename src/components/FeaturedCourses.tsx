d73ee2d345b6396020084aeaeb4f1366f62f9e62
Anushkaa-80 <anushkaa8090@gmail.com>
2025-08-03 01:18:50 +0530

    Add WhyChooseUs and StickyScroll components; update FeaturedCourses layout and styling

--- src/components/FeaturedCourses.tsx
+++ src/components/FeaturedCourses.tsx
@@ -1,20 +1,64 @@
+"use client";
 import React from 'react'
 import { BackgroundGradient } from "./ui/background-gradient";
-import courseData from "../data/music_courses.json"; // Assuming you have a JSON file with course data
+import courseData from "../data/music_courses.json"; 
+import Link from 'next/link';
 
+interface Course {
+    id: number;
+    title: string;
+    slug: string;
+    description: string;
+    price: number;
+    instructor: string;
+    isFeatured: boolean;
+}
 export default function FeaturedCourses() {
-  return (
+       const featuredCourses = courseData.courses.filter((course:Course) =>course.isFeatured)
+
+ 
+    return (
     <div  className="py-12 bg-gray-900" >
         
         <div>
 
             <div className="text-center">
-                <h2>Featured Courses</h2>
-                <p>Learn With the Best</p>
+                <h2 className="text-base text-teal-600 font-semibold tracking-wide uppercase">Featured Courses</h2>
+                <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-white sm:text-4xl">Learn With the Best</p>
             </div>
         </div>
-        <div></div>
-        <div></div>
+        <div className="mt-10">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 justify-center">
+                  
+                {
+                    featuredCourses.map
+                    ((course: Course) => (
+
+                        <div  key ={course.id} className="flex justify-center">
+                            <BackgroundGradient
+                        className="flex flex-col rounded-[22px] bg-zinc-900 text-white overflow-hidden h-full max-w-sm">
+                            <div className="p-4 sm:p-6 flex flex-col items-center text-center flex-grow">
+                                <p className="text-lg sm:text-xl text-gray-300 mt-4 mb-2 dark:text-neutral-200">{course.title}</p>
+                                <p className="text-sm text-shadow-neutral-50 dark:text-neutral-400 flex-grow">{course.description}</p>
+                                <Link href={`/courses/${course.slug}`}>
+                                Learn More
+                                </Link>
+                            </div>
+                        </BackgroundGradient>
+                            
+                        </div>
+
+
+                    ))
+                }
+
+            </div>
+        </div>
+        <div className="mt-20 text-center">
+            <Link href={"/courses"} className="px-4 py-2 rounded border border-teal-600 text-teal-600 hover:bg-teal-600 hover:text-white transition-colors duration-300">
+            View all courses
+            </Link>
+        </div>
         
    
     </div>
